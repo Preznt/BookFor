63bc6db366eec333358d63a06ab2dce622eeb0ee
Preznt <bjw1403@gmail.com>
2022-11-01 10:32:55 +0900

    NodeJS_RequestMapping

--- Node_03/routes/city.js
+++ Node_03/routes/city.js
@@ -105,23 +105,94 @@ router.get("/country", (req, res) => {
 //   }
 // });
 
-router.get("/country/:lt_pop?/:gt_pop", (req, res) => {
-  const gt_pop = req.params.gt_pop;
-  const lt_pop = req.params.lt_pop;
-  const nation =
-    "SELECT * FROM country WHERE GNP >= ? && GNP <= ? ORDER BY GNP DESC";
-  // const oneNation = "SELECT * FROM country WHERE GNP <= ? ORDER BY GNP DESC";
+// router.get("/country/:lt_pop?/:gt_pop", (req, res) => {
+//   const gt_pop = req.params.gt_pop;
+//   const lt_pop = req.params.lt_pop;
+//   const nation =
+//     "SELECT * FROM country WHERE GNP >= ? && GNP <= ? ORDER BY GNP DESC";
 
-  if (lt_pop) {
-    mysql.execute(nation, [lt_pop, gt_pop], (err, result, f) => {
-      res.json(result);
-    });
-  } else {
-    mysql.execute(nation, [0, gt_pop], (err, result, f) => {
-      res.json(result);
-    });
-  }
+//   if (lt_pop) {
+//     mysql.execute(nation, [lt_pop, gt_pop], (err, result, f) => {
+//       res.json(result);
+//     });
+//   } else {
+//     mysql.execute(nation, [0, gt_pop], (err, result, f) => {
+//       res.json(result);
+//     });
+//   }
+// });
+
+// http:loaclahost:3000/country/100/200 에 대한 응답
+// http://localhost:3000/country/100 처럼 요청을 하면
+//    없는 URI 라고 거부(Not Found Error)
+// 두가지 Req 를 처리하기 위하여
+// RequestMapping("/country/...")을 배열로 선언하여
+// 두가지 Req를 일단 모두 받도록 처리한다.
+// Mulit RequestMapping이라고 한다.
+router.get(["/country/:start/:end", "/country/:end"], (req, res) => {
+  // 변수가 2개일때, 또는 변수가 1개일때 어떻게 처리할 것인가
+  // let start = req.params.start;
+  // const end = req.params.end;
+  /**
+   * 객체의 구조분해
+   * req.params 에 있는 sub 속성들 중에서
+   * start, end 를 추출하여 같은 이름의 변수를 생성하고
+   * 그 변수에 값을 저장해 달라
+   */
+  let { start, end } = req.params;
+  console.log(start, end);
+  // if (!start) {
+  //   start = 0;
+  // }
+  start = start || 0;
+  // start 값이 있으면 start 값을 담고 없으면 0을 담아라
+
+  console.log(start, end);
+  const sql = "SELECT * FROM country WHERE gnp BETWEEN ? AND ?";
+  mysql.execute(sql, [start, end], (err, result, field) => {
+    res.json(result);
+  });
 });
+// 선택적 파라메터 RequestMapping
+router.get("/gnp/:start?/:end?", (req, res) => {
+  let { start, end } = req.params;
+  console.log(start, end);
+  // city/gnp/100 처럼 1개의 데이터만 전송을 하면
+  // start = 100, end = undefined 가 담기게 된다
+  // 일단 end 값이 undefined 이면 0으로 세팅을 한다
+  end = end || 0;
+  // city/gnp/100 처럼 1개의 데이터만 전송을 했다면
+  // start =100, end = 0 으로 세팅이 될것이다
+
+  // start 와 end 가 서로 바뀐상태
+  // start 와 end 를 서로 교환하기
+  if (end === 0) {
+    // const _t = start;
+    // start = end;
+    // end = _t;
+    // XOR(배타적 논리연산, 같은값은 0. 다른값은 1인 논리연산)
+    // 을 이용한 두 변수의 값 교환하기(변수의 Swap)
+    start = start ^ end;
+    end = start ^ end;
+    start = start ^ end;
+  }
+
+  const sql = "SELECT * FROM country WHERE gnp BETWEEN ? AND ?";
+  mysql.execute(sql, [start, end], (err, result, feild) => {
+    res.send(result);
+  });
+});
+
+/**
+ * 현재 여기의 요청 처리는 start 변수와 end 변수를 전달받아 처리를 한다
+ * country/100/300 처럼 2개의 변수를 모두 전달하면
+ * start = 100, end = 300 의 값이 변수에 담기게 된다
+ *
+ * 만약
+ * country/100 처럼 1개의 변수만 전달하면
+ * start = undefined, end = 100의 값이 변수에 담기게 된다
+ * 만약 start 가 undefined 이면 start = 0 으로 세팅하면 된다
+ */
 
 // localhost:3000/city/도시이름 이라고 요청을 하면
 // :name 은 입력한 도시이름이 name이라는 변수에 담긴다
