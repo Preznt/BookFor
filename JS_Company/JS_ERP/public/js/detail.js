68eb52644b8272683628e2b77b9b2380f4d06412
Preznt <bjw1403@gmail.com>
2022-11-14 16:43:26 +0900

    나라상사2

--- JS_Company/JS_ERP/public/js/detail.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const infoTable = document.querySelector("table.buyer");
-  infoTable.addEventListener("click", (e) => {
-    const target = e.target;
-    if (target.tagName == "TD") {
-      document.location.href = "/update";
-    }
-  });
-});
