1fd956fe6cbc79bbf40084cb304cbfe3e0c8b133
Preznt <bjw1403@gmail.com>
2023-01-17 14:06:07 +0900

    로그인/로그아웃 book api

---
+++ Node_200_Naver-V3/routes/api/user_api.js
@@ -0,0 +1,11 @@
+import express from "express";
+const router = express.Router();
+
+// 로그인하기
+router.post("/join", (req, res) => {});
+router.post("/login", (req, res) => {});
+
+//로인한 사용자 정보 get 하기
+router.get("/info", (req, res) => {});
+
+export default router;
